0ac235d635c021919539572986fdff114b3231aa
danil.kobeliatskyi <danil.kobeliatskyi@gmail.com>
2018-03-01 16:53:53 +0200

    Replace if logic in switch

--- src/com/danilkobeliatskyi/ContactActor.java
+++ src/com/danilkobeliatskyi/ContactActor.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class ContactActor {
 
     private List<Contact> data;
-    FileManager fileManager;
+    private FileManager fileManager;
 
     public ContactActor(String filename) {
         fileManager = new FileManager(filename);
@@ -27,42 +27,49 @@ public class ContactActor {
 
     public void replaceContactName(String name, String newName){
         for (Contact contact : data) {
-            if (contact.getName().contains(name))
+            if (contact.getName().contains(name)) {
                 contact.setName(newName);
+            }
             fileManager.updateContacts(data);
         }
     }
 
     public void replaceContactPhone(String phone, String newPhone){
         for (Contact contact : data){
-            if (contact.getPhone().contains(phone))
+            if (contact.getPhone().contains(phone)) {
                 contact.setPhone(newPhone);
+            }
             fileManager.updateContacts(data);
         }
     }
 
     public void replaceContactEmail(String email, String newEmail){
         for (Contact contact : data){
-            if (contact.getEmail().contains(email))
+            if (contact.getEmail().contains(email)) {
                 contact.setEmail(newEmail);
+            }
             fileManager.updateContacts(data);
         }
     }
 
     public void deleteContact(String name){
         List<Contact> newContacts = new ArrayList<>();
-        for (Contact contact : data)
-            if (!contact.getName().equals(name))
+        for (Contact contact : data) {
+            if (!contact.getName().equals(name)) {
                 newContacts.add(contact);
+            }
+        }
         data.clear();
         data.addAll(newContacts);
         fileManager.updateContacts(data);
     }
 
     public void findContact(String name){
-        for (Contact contact : data)
-            if (contact.getName().equals(name))
+        for (Contact contact : data){
+            if (contact.getName().equals(name)) {
                 printContact(contact);
+            }
+        }
     }
 
     public void deleteAllContact(){
--- src/com/danilkobeliatskyi/Program.java
+++ src/com/danilkobeliatskyi/Program.java
@@ -16,69 +16,85 @@ public class Program {
         System.out.println("Click 6 to change phone of contact");
         System.out.println("Click 7 to change email of contact");
         System.out.println("Click 8 to find a contact");
-        System.out.println("Click 9 to exit the program");
+        System.out.println("Click 0 to exit the program");
 
-        while (0 == 0){
+        while (true){
 
         String action = scanner.nextLine();
-
-            if(action.equals("1")) {
-                actor.showAllContact();
-            }
-
-            if (action.equals("2")) {
-                System.out.println("Enter name");
-                String name = scanner.nextLine();
-                System.out.println("Enter phone");
-                String phone = scanner.nextLine();
-                System.out.println("Enter email");
-                String email = scanner.nextLine();
-                actor.addContact(name, phone, email);
-            }
-
-            if (action.equals("3")) {
-                System.out.println("Enter name of contact, which you wood to delete");
-                String name = scanner.nextLine();
-                actor.deleteContact(name);
-            }
-
-            if (action.equals("4")) {
-                actor.deleteAllContact();
-            }
-
-            if (action.equals("5")) {
-                System.out.println("Enter old name");
-                String name = scanner.nextLine();
-                System.out.println("Enter new name");
-                String newName = scanner.nextLine();
-                actor.replaceContactName(name, newName);
-            }
-
-            if (action.equals("6")) {
-                System.out.println("Enter old phone");
-                String phone = scanner.nextLine();
-                System.out.println("Enter new phone");
-                String newPhone = scanner.nextLine();
-                actor.replaceContactPhone(phone, newPhone);
-            }
-
-            if (action.equals("7")) {
-                System.out.println("Enter old email");
-                String email = scanner.nextLine();
-                System.out.println("Enter new email");
-                String newEmail = scanner.nextLine();
-                actor.replaceContactEmail(email, newEmail);
-            }
-
-            if (action.equals("8")) {
-                System.out.println("Enter name of contact, which you wood like to find");
-                String name = scanner.nextLine();
-                actor.findContact(name);
-            }
-
-            if (action.equals("0")){
+            switch (action) {
+                case "1" : actor.showAllContact();
                 break;
+
+                case "2" : addContact();
+                break;
+
+                case "3" : deleteContact();
+                    break;
+
+                case "4" : actor.deleteAllContact();
+                    break;
+
+                case "5" : replaceName();
+                    break;
+
+                case "6" : replacePhone();
+                    break;
+
+                case "7" : replaceEmail();
+                    break;
+
+                case "8" : findContact();
+                    break;
+
+                case "0" : System.exit(0);
+                    break;
             }
         }
+    }
+
+    private void addContact(){
+        System.out.println("Enter name");
+        String name = scanner.nextLine();
+        System.out.println("Enter phone");
+        String phone = scanner.nextLine();
+        System.out.println("Enter email");
+        String email = scanner.nextLine();
+        actor.addContact(name, phone, email);
+    }
+
+    private void deleteContact(){
+        System.out.println("Enter name of contact, which you wood to delete");
+        String name = scanner.nextLine();
+        actor.deleteContact(name);
+    }
+
+    private void replaceName(){
+        System.out.println("Enter old name");
+        String name = scanner.nextLine();
+        System.out.println("Enter new name");
+        String newName = scanner.nextLine();
+        actor.replaceContactName(name, newName);
+    }
+
+    private void replacePhone(){
+        System.out.println("Enter old phone");
+        String phone = scanner.nextLine();
+        System.out.println("Enter new phone");
+        String newPhone = scanner.nextLine();
+        actor.replaceContactPhone(phone, newPhone);
+    }
+
+    private void replaceEmail(){
+        System.out.println("Enter old email");
+        String email = scanner.nextLine();
+        System.out.println("Enter new email");
+        String newEmail = scanner.nextLine();
+        actor.replaceContactEmail(email, newEmail);
+    }
+
+    private void findContact(){
+        System.out.println("Enter name of contact, which you wood like to find");
+        String name = scanner.nextLine();
+        actor.findContact(name);
     }
 }
